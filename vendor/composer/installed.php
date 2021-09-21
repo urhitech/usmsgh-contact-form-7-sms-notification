6381ef88a6a7ebbb5bf498f80324b08a6cf1f5c6
urhitech <admin@urhitech.com>
2021-09-21 13:10:31 +0000

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cf56f071c8388162e92bc6910cd7cb9a74fc4f08',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cf56f071c8388162e92bc6910cd7cb9a74fc4f08',
+    ),
+    'urhitech/urhitech-sms-php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'bed188a9d60c061bdeca48f72c9a70995f9c24d3',
+    ),
+  ),
+);
